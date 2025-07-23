b7fb72878a377d91657173793d7e0b5fb3872a01
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-23 14:46:50 +0500

    JavaScript 2025

--- 02 - Let Var Const/script.js
+++ 02 - Let Var Const/script.js
@@ -1,18 +1,23 @@
 // var and let are used to define a variable and a blocked scoped variable
-// const means the constant value; these values cannot be change throughout the code
-
+// Let also doesnt allow to redeclare the value; but i can add the value in the same variable later by removing the let
 
 let boy_name = "Shaheer Jamal";
 boy_name = "Abdul Basit";
 let age = 19;
+
+// const means the constant value; these values cannot be change throughout the code
 const language = "Python";
+// language = "Urdu" -- This line is going to throw an error bc i can not redeclare the const 
 
 // console.log(boy_name, "is a ", age, "year old", language, "developer")
 // console.log(`${boy_name} is a ${age} year old developer who has the command on ${language}`)
 
-// {
-//     let boy_name = "Abdul Basit"
-//     console.log(boy_name)
-// }
+
+// To create  an Object in Js
+const obj = {
+                "City":"Karachi",
+                "City":"Lahore",
+                "City":"Islamabad"    
+            }
 
 console.log(boy_name)
