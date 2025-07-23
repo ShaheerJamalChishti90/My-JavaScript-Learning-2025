04325b9fcc09fbf039d31dc0a71c239f02c4188f
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-23 12:53:39 +0500

    JavaScript 2025

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Here we will learn how to create variables and what are the data types??
 
 let x = 5 
-x = "ShaheerJamal"
-// let y = 5
-
 console.log(x)
+x = "ShaheerJamal"
+console.log(x)
+// let y = 5
