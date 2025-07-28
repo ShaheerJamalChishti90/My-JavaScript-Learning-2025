26def6531d9aaef45e39e2a7cf718802695c94ca
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-28 17:18:02 +0500

    JavaScript 2025

---
+++ Chapter 02/Operators & Expressions.js
@@ -0,0 +1,76 @@
+// Operators in JS
+
+
+// Arithmetic Operators
+// + - * / %  ** ++ --
+
+// let x = 2 
+// let y = 4
+
+// console.log(x + y)
+// console.log(x - y)
+// console.log(x * y)
+// console.log(x / y)
+// console.log(x % y)
+// console.log(x ** y)
+// console.log(x ++ y)
+// console.log(x -- y)
+
+// let a = 7 
+// let b = 3
+
+// console.log(a % b)
+
+
+// console.log(`The value of x++ is ${x++}`)
+// console.log(`1: The value of x is ${x}`)
+// console.log(`2: if I do x++ then the value is still ${x++}`)
+// console.log(`3: No, but the value of x in the backend is already incremented! and now if you print x so the value is  ${x}`)
+
+// console.log("-------------------------------------------")
+
+// console.log(`But the value of ++x is ${++x}`)
+
+
+// Assignment Operators
+// = -= += *= /= 
+
+// let x = 30
+
+// console.log(x += 5 )
+// console.log(x -= 5)
+// console.log(x *= 5)
+// console.log(x /= 5) 
+
+// Comparision Operators
+// > < >= <= != == === ?
+
+let a = 5
+let b =  10
+
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+
+console.log(5 != 3);     // true, because 5 is not equal to 3
+console.log(5 != '5');   // false, because values are equal (type is ignored)
+
+console.log(5 == '5');   // true, because values are equal (type doesn't matter)
+console.log(0 == false); // true, because JavaScript considers them equal
+console.log(null == undefined); // true, because both represent "no value"
+
+
+console.log(5 === 5);     // true, same value and same type
+console.log(5 === '5');   // false, same value but different type (number vs string)
+console.log(true === 1);  // false, true is boolean, 1 is number
+
+
+console.log(5 !== '5');   // true, value is same but type is different
+console.log(3 !== 3);     // false, both value and type are same
+
+
+let age = 18;
+let canVote = (age >= 18) ? "Yes" : "No";
+// If age is 18 or more, canVote will be "Yes", otherwise "No"
+console.log(canVote); // "Yes"
