cdd17dbe05be59dc41c6b581afede87a2e601a88
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-08-14 01:05:47 +0500

    JavaScriipt 2025

---
+++ Chapter 03/While Loop.js
@@ -0,0 +1,43 @@
+// let i = 0; 
+// while (i <= 10) {
+//     console.log("Hey! This is", i)
+//     i ++
+// }
+// console.log("The program is end here! Thankyou for using our code")
+
+// let i = 100; 
+// while (i >= 50) {
+//     i --
+//     if (i == 78){
+//         console.log("This is the 78th Birthday of Pakistan")
+//     }
+//     else {
+//         console.log("Hey! This is", i)
+//     }
+// }
+// console.log("The program is end here! Thankyou for using our code")
+
+
+// while (true) {
+//     let i = 0;
+//     i ++ 
+//     if (i < 10) {
+//         console.log(i)
+//     }
+//     else {
+//         console.log("The code is finished and I is no longer greater than 10!")
+//     };
+// }
+
+
+
+// DO WHILE LOOP!
+var i = 101;
+do {
+ console.log(i);
+} while (i < 100, i --){
+    if (i == 50) {
+        console.log("")
+    }
+};
+
