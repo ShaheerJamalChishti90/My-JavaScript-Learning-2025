f05f3a27a2df58bae4d2cb7d410b40da739e4790
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-08-20 14:39:53 +0500

    JavaScript 2025

--- Chapter 03/Do While.js
+++ Chapter 03/Do While.js
@@ -3,12 +3,27 @@
 // and keep asking until they say "blue".
 
 
-let user_color = prompt("Color here: ")
+// let user_color = "red"
+// do {
+//     if (user_color == "blue"){
+//         console.log("Exiting the code")
+//         break;
+//     } else {
+//         console.log(`User choose ${user_color}, not "Blue"`)
+//     }
+// } while (user_color == "blue");
+
+
+// Start with number 50 and keep subtracting 5 until it’s less than 10.
+
+// let n = 50
+// do {
+//     console.log(n --) 
+// } while (n >= 10)
+
+
+// Start with 0 and keep adding 3 until you pass 20.
+let x = 0
 do {
-    if (user_color == "blue"){
-        console.log("Exiting the code")
-        break;
-    } else {
-        console.log(`User choose ${user_color}, not "Blue"`)
-    }
-} while (user_color == "blue");
+    console.log(x += 3)
+} while (x < 20)
