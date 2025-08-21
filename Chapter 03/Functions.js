bbd8c4a494d81a383b6d16b0f396e3540d7f5ed9
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-08-22 01:47:49 +0500

    JavaScript 2025

---
+++ Chapter 03/Functions.js
@@ -0,0 +1,24 @@
+// Function without paremeters
+// function Greet(){
+//     // console.log("Hellow Shaheer")
+//     return "This is a return statement"
+// }
+
+// console.log(Greet())
+// Greet()
+
+// Function with Paremeters
+// function add(a, b) {
+//   return a + b;
+// }
+
+// Arrow Functions
+// const add = (a, b) => a + b;
+// console.log(add(5, 2))
+
+
+const greet = (name) => {
+    return `Hey ${name}! This is Melenie, How are you doing?`
+}
+
+console.log(greet("Shaheer"))
